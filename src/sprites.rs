3665b7ba6a6cf928c664f9de1e6292f8801f6311
joshuakelly <inst.zombie@gmail.com>
2024-03-25 09:52:42 -0700

    Added scaling for sprites

--- src/sprites.rs
+++ src/sprites.rs
@@ -1,5 +1,5 @@
 use macroquad::color::YELLOW;
-use macroquad::math::{DMat2, DVec2};
+use macroquad::math::{DMat2, DVec2, DVec3};
 use macroquad::prelude::mat2;
 use crate::fpv::FirstPersonViewer;
 use crate::level::Level;
@@ -10,7 +10,7 @@ pub enum SpriteType {
 
 pub struct Sprites {
     pub sp_positions: Vec<DVec2>, // midpoint sprite position
-    sp_size: Vec<DVec2>, // Describes width, height of sprite, position is midpoint
+    sp_size: Vec<DVec3>, // Describes width, height of sprite, position is midpoint
     sp_type: Vec<SpriteType>,
     sp_distance: Vec<f64> // Distance from player
 }
@@ -35,7 +35,7 @@ impl Sprites {
     pub fn add_sprite(&mut self, pos: DVec2, sprite_type: SpriteType) {
         self.sp_positions.push(pos);
         self.sp_type.push(sprite_type);
-        self.sp_size.push(DVec2::from((1.0, 1.0)));
+        self.sp_size.push(DVec3::from((0.1, 0.1, 0.0))); // x scale, y scale, x offset
         self.sp_distance.push(f64::INFINITY);
     }
     pub fn draw_sprites(
@@ -52,21 +52,21 @@ impl Sprites {
 
         // TODO: Frustum culling
         // TODO: Sort sprites based on distance
-        for sprite in self.sp_positions.iter() {
+        for (sprite, sprite_scale) in self.sp_positions.iter().zip(self.sp_size.iter()) {
             let sprite_rel_pos = *sprite - pos;
             let transform = camera_inverse.mul_vec2(sprite_rel_pos);
             if transform.y >= 0.0 {
                 let sprite_screen_y = (h /2.0) * (1.0 + transform.x/ transform.y);
 
                 // Calculate width of the sprite
-                let sprite_width = (w / transform.y).abs();
+                let sprite_width = ((sprite_scale.x*w) / transform.y).abs();
                 // Calculate the left and right pixel to fill in
-                let offset_x = w/2.0;
+                let offset_x = (w/2.0)*(1.0 + sprite_scale.z);
                 let draw_start_x = 0.0f64.max((-1.0*sprite_width/2.0) + offset_x) as usize;
                 let draw_end_x = (rw - 1).min((sprite_width / 2.0 + offset_x) as usize);
 
                 // Calculate height of sprite
-                let sprite_height = (h / transform.y).abs();
+                let sprite_height = sprite_scale.y*(h / transform.y).abs();
                 let draw_start_y = 0.0f64.max((-1.0*sprite_height/2.0) + sprite_screen_y) as usize;
                 let draw_end_y = (rh - 1).min((sprite_height / 2.0 + sprite_screen_y) as usize);
                 //let yc = (fpv.render_size.1 as usize - 1).min(0.0f64.max(sprite_screen_y) as usize);
