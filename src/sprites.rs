0255a56ad2246183b6d1991d6e87a1713565160f
joshuakelly <inst.zombie@gmail.com>
2024-03-25 10:44:47 -0700

    Laid foundatiopn for sprite sorting

--- src/sprites.rs
+++ src/sprites.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use macroquad::color::{Color, YELLOW};
 use macroquad::math::{DMat2, DVec2, DVec3};
 use macroquad::prelude::mat2;
@@ -60,45 +61,46 @@ impl Sprites {
             }
         }
         self.sp_draw_order.sort_by(|a,b| {
-            b.0.partial_cmp(&a.0).unwrap()
+            match b.0.partial_cmp(&a.0) {
+                None => {debug_assert!(false);Ordering::Equal} // Prefer nonsense over crashing
+                Some(x) => {x}
+            }
         });
 
-        println!("{:?}", self.sp_draw_order);
-
         // TODO: Frustum culling
-        // TODO: Sort sprites based on distance
-        for (sprite, sprite_scale) in self.sp_positions.iter().zip(self.sp_size.iter()) {
+        for (sprite, sprite_scale) in self.sp_draw_order.iter().map(|x| {
+                let (_, i) = *x;
+                (&self.sp_positions[i], self.sp_size[i])
+        }) {
             let sprite_rel_pos = *sprite - pos;
             let transform = camera_inverse.mul_vec2(sprite_rel_pos);
-            if transform.y >= 0.0 {
-                let sprite_screen_y = (h /2.0) * (1.0 + transform.x/ transform.y);
+            let sprite_screen_y = (h /2.0) * (1.0 + transform.x/ transform.y);
 
-                // Calculate width of the sprite
-                let sprite_width = ((sprite_scale.x*w) / transform.y).abs();
-                // Calculate the left and right pixel to fill in
-                let offset_x = (w/2.0)*(1.0 + sprite_scale.z);
-                let draw_start_x = 0.0f64.max((-1.0*sprite_width/2.0) + offset_x) as usize;
-                let draw_end_x = (rw - 1).min((sprite_width / 2.0 + offset_x) as usize);
+            // Calculate width of the sprite
+            let sprite_width = ((sprite_scale.x*w) / transform.y).abs();
+            // Calculate the left and right pixel to fill in
+            let offset_x = (w/2.0)*(1.0 + sprite_scale.z);
+            let draw_start_x = 0.0f64.max((-1.0*sprite_width/2.0) + offset_x) as usize;
+            let draw_end_x = (rw - 1).min((sprite_width / 2.0 + offset_x) as usize);
 
-                // Calculate height of sprite
-                let sprite_height = sprite_scale.y*(h / transform.y).abs();
-                let draw_start_y = 0.0f64.max((-1.0*sprite_height/2.0) + sprite_screen_y) as usize;
-                let draw_end_y = (rh - 1).min((sprite_height / 2.0 + sprite_screen_y) as usize);
-                //let yc = (fpv.render_size.1 as usize - 1).min(0.0f64.max(sprite_screen_y) as usize);
+            // Calculate height of sprite
+            let sprite_height = sprite_scale.y*(h / transform.y).abs();
+            let draw_start_y = 0.0f64.max((-1.0*sprite_height/2.0) + sprite_screen_y) as usize;
+            let draw_end_y = (rh - 1).min((sprite_height / 2.0 + sprite_screen_y) as usize);
+            //let yc = (fpv.render_size.1 as usize - 1).min(0.0f64.max(sprite_screen_y) as usize);
 
-                let rd = fpv.render_image.get_image_data_mut();
+            let rd = fpv.render_image.get_image_data_mut();
 
-                for y in draw_start_y..=draw_end_y {
-                    if transform.y < fpv.z_buffer[y] {
-                        for x in draw_start_x..=draw_end_x {
-                            let c = (transform.y / fpv.z_buffer[y]) as f32;
-                            rd[y*rw + x] = Color::new(c, 1.0, 1.0, 1.0).into();
-                        }
+            for y in draw_start_y..=draw_end_y {
+                if transform.y < fpv.z_buffer[y] {
+                    for x in draw_start_x..=draw_end_x {
+                        let c = (transform.y / fpv.z_buffer[y]) as f32;
+                        rd[y*rw + x] = Color::new(c, 1.0, 1.0, 1.0).into();
                     }
                 }
-                println!("(draw_start_y, draw_end_y) {:?}, sprite_screen_y {}, sprite_width {}, transform.y {}, draw_start_x {}, draw_end_x {}",
-                         (draw_start_y, draw_end_y), sprite_screen_y, sprite_width, transform.y, draw_start_x, draw_end_x);
             }
+            //println!("(draw_start_y, draw_end_y) {:?}, sprite_screen_y {}, sprite_width {}, transform.y {}, draw_start_x {}, draw_end_x {}",
+            //         (draw_start_y, draw_end_y), sprite_screen_y, sprite_width, transform.y, draw_start_x, draw_end_x);
         }
     }
 }
