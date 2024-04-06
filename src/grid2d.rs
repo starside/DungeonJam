832e5bfaaf0df999895d160c7454ec22cf3003b9
joshuakelly <inst.zombie@gmail.com>
2024-04-05 19:09:18 -0700

    Fixed yet another boundary cindition issue

--- src/grid2d.rs
+++ src/grid2d.rs
@@ -46,7 +46,7 @@ impl<T: Serialize + DeserializeOwned + Default> Grid2D<T>{
             return None;
         }
         let x = if pos.x < 0 {
-            pos.x.abs() as usize % self.width
+            self.width - ((-1*pos.x) as usize % self.width)
         } else {
             pos.x as usize % self.width
         };
